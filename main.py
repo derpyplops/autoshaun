8a310675cbde8af2ba430404594df574d2b6b0c3
jon <jonathan.n@u.nus.edu>
2024-03-04 19:04:12 +0700

    WIP refactor generate embeddings

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import os
 import PyPDF2
 import numpy as np
 import openai
-import tiktoken
-import pandas as pd
 from openai.embeddings_utils import distances_from_embeddings
 from rich import print
-from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
 import datasets
 from time import time
 
@@ -42,94 +38,6 @@ def textify_pdf(filepath):
         for page in pdf.pages:
             text += page.extract_text()
     return text
-
-
-max_tokens = 500
-
-# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
-tokenizer = tiktoken.get_encoding("cl100k_base")
-
-
-# Function to split the text into chunks of a maximum number of tokens
-def split_into_many(text, max_tokens=max_tokens):
-    # Split the text into sentences
-    sentences = text.split('. ')
-
-    # Get the number of tokens for each sentence
-    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
-
-    chunks = []
-    tokens_so_far = 0
-    chunk = []
-
-    # Loop through the sentences and tokens joined together in a tuple
-    for sentence, token in zip(sentences, n_tokens):
-
-        # If the number of tokens so far plus the number of tokens in the current sentence is greater
-        # than the max number of tokens, then add the chunk to the list of chunks and reset
-        # the chunk and tokens so far
-        if tokens_so_far + token > max_tokens:
-            chunks.append(". ".join(chunk) + ".")
-            chunk = []
-            tokens_so_far = 0
-
-        # If the number of tokens in the current sentence is greater than the max number of
-        # tokens, go to the next sentence
-        if token > max_tokens:
-            continue
-
-        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
-        chunk.append(sentence)
-        tokens_so_far += token + 1
-
-    return chunks
-
-
-def extract_and_get_token_counts(txt_file_paths):
-    token_counts = []
-
-    for path in txt_file_paths:
-        text = open(path, 'r').read()
-        token_count = len(text.split())  # Count tokens by splitting on whitespaces
-        token_counts.append(token_count)
-
-    return token_counts
-
-
-def extract_and_write_all_pdfs_to_txt():
-    pdf_file_paths = [os.path.join('data/pdfs', f) for f in os.listdir('data/pdfs')]
-
-    for file in pdf_file_paths:
-        text = textify_pdf(file)
-        # write text to same filename but .txt
-        new_filename = file.replace('.pdf', '.txt').replace('pdfs', 'txts')
-        # create if not exists
-        os.makedirs(os.path.dirname(new_filename), exist_ok=True)
-        with open(new_filename, 'w') as f:
-            f.write(text)
-
-
-def chunkify_and_write():
-    text_file_paths = [os.path.join('data/txts', f) for f in os.listdir('data/txts')]
-    # print(extract_and_get_token_counts(text_file_paths))
-    texts = [open(path, 'r').read() for path in text_file_paths]
-    data = []
-    chunk_id = 0
-    for i, text in enumerate(texts):
-        chunks = split_into_many(text)
-        for j, chunk in enumerate(chunks):
-            data.append(
-                {
-                    "chunk_id": chunk_id,
-                    "textfilepath": text_file_paths[i],
-                    "chunk_position": j,
-                    "chunk": chunk,
-                }
-            )
-            chunk_id += 1
-
-    df = pd.DataFrame(data)
-    df.to_csv('chunks.csv')
 
 
 def create_context(
@@ -186,7 +94,6 @@ def _answer_question(
     prompt = f"Context: {context}\n\nPlease help me with my question, but it is very important that you " \
              f"do not make answers up. If you do not know the answer, say 'I don\'t know.'\n\nQuestion:" \
              f" {question}"
-    # If debug, print the raw model response
     if debug:
         print(prompt)
         print("\n\n")
@@ -197,8 +104,6 @@ def _answer_question(
             messages=[
                 {"role": "system", "content": "You are a helpful financial assistant assisting a customer with their "
                                               "finances."},
-                # {"role": "user", "content": "Who won the world series in 2020?"},
-                # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
                 {"role": "user", "content": prompt}
             ]
         )
@@ -208,34 +113,9 @@ def _answer_question(
         return ""
 
 
-def generate_embeddings(chunk):
-    return openai.Embedding.create(input=chunk, engine='text-embedding-ada-002')['data'][0]['embedding']
-
-
-def chunks_to_embeddings():
-    # load df
-    df = pd.read_csv('data/chunks.csv')
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        df['embeddings'] = list(tqdm(executor.map(generate_embeddings, df['chunk']), total=len(df)))
-    df.to_csv('embeddings.csv')
-
-
 def answer_all_questions(questions):
     for question in questions:
         print(f"[blue]Q: {question}[/blue]\n[magenta]A: {answer_question(question)}[/magenta]\n\n")
-
-
-def add_n_tokens():
-    df = pd.read_csv('embeddings.csv', index_col=0)
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    df['n_tokens'] = df['chunk'].apply(lambda x: len(tokenizer.encode(x)))
-    df.to_csv('embeddings.csv')
-
-
-def upload_to_hf():
-    csv = pd.read_csv('data/embeddings.csv', index_col=0)
-    dataset = datasets.Dataset.from_pandas(csv)
-    dataset.push_to_hub('autoshaun-embeddings', private=True)
 
 
 embeddings_df = load_embeddings()
---
+++ scripts/generate_embeddings.py
@@ -0,0 +1,111 @@
+import os
+import openai
+import tiktoken
+import pandas as pd
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import datasets
+from pathlib import Path
+
+max_tokens = 500
+
+# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+tokenizer = tiktoken.get_encoding("cl100k_base")
+
+
+# Function to split the text into chunks of a maximum number of tokens
+def split_into_many(text, max_tokens=max_tokens):
+    # Split the text into sentences
+    sentences = text.split('. ')
+
+    # Get the number of tokens for each sentence
+    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+
+    chunks = []
+    tokens_so_far = 0
+    chunk = []
+
+    # Loop through the sentences and tokens joined together in a tuple
+    for sentence, token in zip(sentences, n_tokens):
+
+        # If the number of tokens so far plus the number of tokens in the current sentence is greater
+        # than the max number of tokens, then add the chunk to the list of chunks and reset
+        # the chunk and tokens so far
+        if tokens_so_far + token > max_tokens:
+            chunks.append(". ".join(chunk) + ".")
+            chunk = []
+            tokens_so_far = 0
+
+        # If the number of tokens in the current sentence is greater than the max number of
+        # tokens, go to the next sentence
+        if token > max_tokens:
+            continue
+
+        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+        chunk.append(sentence)
+        tokens_so_far += token + 1
+
+    return chunks
+
+def extract_and_get_token_counts(txt_file_paths):
+    token_counts = []
+
+    for path in txt_file_paths:
+        text = open(path, 'r').read()
+        token_count = len(text.split())  # Count tokens by splitting on whitespaces
+        token_counts.append(token_count)
+
+    return token_counts
+
+def chunkify_and_write(in_dir):
+    text_file_paths = [os.path.join(in_dir, f) for f in os.listdir(in_dir)]
+    # print(extract_and_get_token_counts(text_file_paths))
+    texts = [open(path, 'r').read() for path in text_file_paths]
+    data = []
+    chunk_id = 0
+    for i, text in enumerate(texts):
+        chunks = split_into_many(text)
+        for j, chunk in enumerate(chunks):
+            data.append(
+                {
+                    "chunk_id": chunk_id,
+                    "textfilepath": text_file_paths[i],
+                    "chunk_position": j,
+                    "chunk": chunk,
+                }
+            )
+            chunk_id += 1
+
+    df = pd.DataFrame(data)
+    return df
+
+def generate_embeddings(chunk):
+    return openai.Embedding.create(input=chunk, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+
+def add_embeddings(df):
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        df['embeddings'] = list(tqdm(executor.map(generate_embeddings, df['chunk']), total=len(df)))
+    return df
+
+
+def add_n_tokens(df):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    df['n_tokens'] = df['chunk'].apply(lambda x: len(tokenizer.encode(x)))
+    return df
+
+
+def upload_to_hf(df, repo_id):
+    # TODO will this work???????
+    dataset = datasets.Dataset.from_pandas(df)
+    dataset.push_to_hub(repo_id, private=True)
+
+
+if __name__ == "__main__":
+    in_dir = Path.cwd().parent / 'data/txts'
+    repo_id = 'autoshaun-embeddings'
+    chunks_df = chunkify_and_write(in_dir)
+    chunks_df = add_embeddings(chunks_df)
+    chunks_df = add_n_tokens(chunks_df)
+    upload_to_hf(repo_id, chunks_df)
+    print('Done')
